cc208b4ce2b4727b97fc89e66cc1d1d982a64efd
Tobias Samsøe Sørensen <tobi8380@gmail.com>
2024-07-22 14:19:33 +0200

    first working iteration

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,64 @@ use fraction::Fraction;
 
 // May fail if input is not correctly formed
 // pub fn evaluate_expression(exp: &str) -> Result<Fraction, ParseIntError> {
-pub fn evaluate_expression(exp: &str) -> Fraction {
-    let tokens = Tokenizer::new(exp);
-    parse_exp(tokens)
+pub fn eval_expression(exp: &str) -> Fraction {
+    let mut tokens = Tokenizer::new(exp);
+    parse_exp(&mut tokens)
 }
 
-fn parse_exp(tokens: Tokenizer) -> Fraction {
-    todo!()
+fn parse_exp(tokens: &mut Tokenizer) -> Fraction {
+    let mut result = parse_mult(tokens);
+    loop {
+        match tokens.head() {
+            Some("+") => {
+                tokens.next();
+                result = result + parse_mult(tokens)
+            }
+            Some("-") => {
+                tokens.next();
+                result = result - parse_mult(tokens)
+            }
+            _ => break,
+        };
+    }
+    result
 }
 
-fn parse_mult(tokens: Tokenizer) -> Fraction {
-    todo!()
+fn parse_mult(tokens: &mut Tokenizer) -> Fraction {
+    let mut result = parse_factor(tokens);
+    loop {
+        match tokens.head() {
+            Some("*") => {
+                tokens.next();
+                result = result * parse_factor(tokens)
+            }
+            Some("/") => {
+                tokens.next();
+                result = result / parse_factor(tokens)
+            }
+            _ => break,
+        };
+    }
+    result
 }
 
-fn parse_factor(tokens: Tokenizer) -> Fraction {
-    todo!()
+fn parse_factor(tokens: &mut Tokenizer) -> Fraction {
+    match tokens.head() {
+        Some("(") => {
+            tokens.next();
+            parse_exp(tokens)
+        }
+        // TODO: maybe this would actually work with i64
+        Some(int) => match int.parse::<u64>() {
+            Ok(value) => {
+                tokens.next();
+                Fraction::from_int(value as i64)
+            }
+            // TODO: Bubble error instead?
+            Err(_) => panic!("Factor should be positive integer"),
+        },
+        None => panic!("Ill formed expression"),
+    }
 }
 
 struct Tokenizer<'a> {
@@ -56,5 +99,31 @@ mod tests {
         let mut t = Tokenizer::new("hello how are you");
         assert!(t.has_next());
         assert_eq!(t.head(), Some("hello"));
+        assert_eq!(t.next(), Some("hello"));
+        assert_eq!(t.head(), Some("how"));
+        assert_eq!(t.next(), Some("how"));
+        assert_eq!(t.head(), Some("are"));
+        assert_eq!(t.next(), Some("are"));
+        assert_eq!(t.head(), Some("you"));
+        assert_eq!(t.next(), Some("you"));
+        assert!(!t.has_next());
+        assert_eq!(t.head(), None);
+        assert_eq!(t.next(), None);
+    }
+
+    #[test]
+    fn simple_expressions() {
+        assert_eq!(eval_expression("2 + 2"), Fraction::from_int(4));
+        assert_eq!(eval_expression("2 + 6 + 3 + 9"), Fraction::from_int(20));
+        assert_eq!(eval_expression("2 * 3 + 5"), Fraction::from_int(11));
+        assert_eq!(eval_expression("5 - 8"), Fraction::from_int(-3));
+        assert_eq!(
+            eval_expression("3 * 7 + 4 * ( 4 - 2 )"),
+            Fraction::from_int(29)
+        );
+        assert_eq!(
+            eval_expression("9 / 3 * 9 - 8 * 2 / ( 15 / 3 * 8 )"),
+            Fraction::new(266, 10)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use calculator::eval_expression;
+use std::io::{self, Write};
+
+fn main() {
+    println!("Enter an expression to evaluate.");
+    println!("NOTE: All tokens must be separated by spaces.");
+    loop {
+        let mut input = String::new();
+        print!("-> ");
+        let _ = io::stdout().flush(); // Ensure that -> is printed
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let exp = input.trim();
+        println!(" = {}", eval_expression(exp));
+    }
+}
